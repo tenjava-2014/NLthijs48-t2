63624981b908bd17d012371c9200d473d7684bdd
Thijs Wiefferink <nlthijs48@gmail.com>
2014-07-12 11:59:29 +0200

    Adds methods for converting location to map and the other way around (for the config)

--- src/main/java/com/tenjava/entries/NLthijs48/t2/Utils.java
+++ src/main/java/com/tenjava/entries/NLthijs48/t2/Utils.java
@@ -1,5 +1,9 @@
 package com.tenjava.entries.NLthijs48.t2;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
 public class Utils {
@@ -36,4 +40,33 @@ public class Utils {
 		return result;
 	}
 	
+	public static Map<String, Object> locationToMap(Location location) {
+		HashMap<String, Object> result = new HashMap<String, Object>();
+		result.put("world", location.getWorld().getName());
+		result.put("x", location.getX());
+		result.put("y", location.getY());
+		result.put("z", location.getZ());
+		result.put("yaw", location.getYaw());
+		result.put("pitch", location.getPitch());		
+		return result;
+	}
+	
+	public static Location mapToLocation(Map<String, Object> map) {
+		return new Location(
+				Bukkit.getWorld((String)map.get("world")), 
+				(Double)map.get("x"), 
+				(Double)map.get("y"), 
+				(Double)map.get("z"), 
+				(Float)map.get("yaw"), 
+				(Float)map.get("pitch"));
+	}
+	
 }
+
+
+
+
+
+
+
+

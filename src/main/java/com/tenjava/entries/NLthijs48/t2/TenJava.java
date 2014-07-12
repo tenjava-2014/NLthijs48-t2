f9ff3d55e9297f66bd08bd4f19bec37571494f80
Thijs Wiefferink <nlthijs48@gmail.com>
2014-07-12 11:05:52 +0200

    Hello World

--- src/main/java/com/tenjava/entries/NLthijs48/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/NLthijs48/t2/TenJava.java
@@ -2,4 +2,17 @@ package com.tenjava.entries.NLthijs48.t2;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+	
+	
+	
+	public void onEnable() {
+		this.getLogger().info("Hello World");
+	}
+	
+	
+	
+	
+	
+	
+}
